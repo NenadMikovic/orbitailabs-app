ffcd36d7490bfbb9d3d30115d04b50de370616f3
Nenad Mikovic <nenad.mikovic@outlook.com>
2025-06-12 18:15:55 +0200

    login update to Supabase

--- src/app/auth/jwt/sign-in/page.jsx
+++ src/app/auth/jwt/sign-in/page.jsx
@@ -1,11 +1,12 @@
 import { CONFIG } from 'src/global-config';
 
-import { JwtSignInView } from 'src/auth/view/jwt';
+import { SupabaseSignInView } from 'src/auth/view/supabase';
+
 
 // ----------------------------------------------------------------------
 
 export const metadata = { title: `Sign in | Jwt - ${CONFIG.appName}` };
 
 export default function Page() {
-  return <JwtSignInView />;
+  return <SupabaseSignInView />;
 }
