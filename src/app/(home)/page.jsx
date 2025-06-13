90473fbd8dc2dbafd97e483dc8744aa3cf334c06
Nenad Mikovic <nenad.mikovic@outlook.com>
2025-06-13 02:32:17 +0200

    homepage redirect

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,13 +1,6 @@
-import { HomeView } from 'src/sections/home/view';
-
-// ----------------------------------------------------------------------
-
-export const metadata = {
-  title: 'Minimals UI: The starting point for your next project',
-  description:
-    'The starting point for your next project with Minimal UI Kit, built on the newest version of Material-UI ©, ready to be customized to your style',
-};
+import { redirect } from 'next/navigation';
 
 export default function Page() {
-  return <HomeView />;
+  redirect('/dashboard');
 }
+
